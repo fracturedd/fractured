c2252adc92699dc602228a1edc5607b89e02a352
nate <nathanielmeattey@gmail.com>
2026-02-16 18:11:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    move: { speed: 1 },
+    line_linked: { enable: true, opacity: 0.2 },
+    opacity: { value: 0.5 }
+  }
+});
